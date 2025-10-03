404eeb36aa8374d6881e7d84557dbe7903068f36
Marian Polyak <info@neo-lines.com>
2025-10-03 05:25:07 +0300

    Change 123

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ import nextI18nextConfig from './next-i18next.config.js';
 const nextConfig = {
     i18n: nextI18nextConfig.i18n,
     trailingSlash: false,
+    async redirects() {
+        return [
+            {
+                source: '/en',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/en/:path*',
+                destination: '/:path*',
+                permanent: true,
+            },
+        ];
+    },
     images: {
         domains: ['drtunitski.neo-lines.bond'],
     },
