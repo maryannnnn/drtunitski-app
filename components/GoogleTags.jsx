22cb5d73fb3e3da0c495c674b4d6b4116174f07c
Marian Polyak <info@neo-lines.com>
2025-11-23 03:18:06 +0200

    Changed Googlr Code

--- components/GoogleTags.jsx
+++ components/GoogleTags.jsx
@@ -4,41 +4,26 @@ import Script from 'next/script';
 const GoogleTags = () => {
     return (
         <>
-            {/* Google Ads Tag */}
             <Script
-                id="google-ads"
-                strategy="afterInteractive"
-                src="https://www.googletagmanager.com/gtag/js?id=AW-17706912095"
-            />
-            <Script
-                id="google-ads-config"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17706912095');
-          `,
-                }}
-            />
-
-            {/* Google Analytics */}
-            <Script
-                id="google-analytics"
+                id="google-tag-manager"
                 strategy="afterInteractive"
                 src="https://www.googletagmanager.com/gtag/js?id=G-V6ZF4RL4ST"
             />
             <Script
-                id="google-analytics-config"
+                id="google-tags-config"
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-V6ZF4RL4ST');
-          `,
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        
+                        // Конфигурация Google Analytics
+                        gtag('config', 'G-V6ZF4RL4ST');
+                        
+                        // Конфигурация Google Ads
+                        gtag('config', 'AW-17706912095');
+                    `,
                 }}
             />
         </>
