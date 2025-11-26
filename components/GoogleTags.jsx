28c2a5621ca774d0085da9d3ae157c8ee7f80369
Marian Polyak <info@neo-lines.com>
2025-11-26 04:12:37 +0200

    Added code Yandex

--- components/GoogleTags.jsx
+++ components/GoogleTags.jsx
@@ -4,6 +4,7 @@ import Script from 'next/script';
 const GoogleTags = () => {
     return (
         <>
+            {/* Google Tags */}
             <Script
                 id="google-tag-manager"
                 strategy="afterInteractive"
@@ -26,6 +27,42 @@ const GoogleTags = () => {
                     `,
                 }}
             />
+
+            {/* Yandex.Metrika */}
+            <Script
+                id="yandex-metrika"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(m,e,t,r,i,k,a){
+                            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+
+                        ym(105509619, 'init', {
+                            ssr:true, 
+                            webvisor:true, 
+                            clickmap:true, 
+                            ecommerce:"dataLayer", 
+                            accurateTrackBounce:true, 
+                            trackLinks:true
+                        });
+                    `,
+                }}
+            />
+
+            {/* Noscript для Яндекс.Метрики */}
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/105509619"
+                        style={{position:'absolute', left:'-9999px'}}
+                        alt=""
+                    />
+                </div>
+            </noscript>
         </>
     );
 };
